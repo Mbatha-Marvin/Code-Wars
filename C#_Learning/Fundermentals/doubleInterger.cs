51823978c3b6d1caaececcd5d470228941025c45
Mbatha-Marvin <mbathamarvin597@gmail.com>
2022-08-14 21:58:09 +0300

    initializing my files

---
+++ C#_Learning/Fundermentals/doubleInterger.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Kata
+{
+  public static int DoubleInteger(int n)
+  {
+    //Double the input!
+    return n*2;
+  }
+}
---
+++ C#_Learning/Fundermentals/helloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Kata
+{
+  // Write a public static method "greet" that returns "hello world!"
+  public static string greet()
+  {
+    return "hello world!";
+  }
+}
+
